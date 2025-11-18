93621f7b0bcefa0781465a487a1ce29e11857c7c
snazziington <amini4life@gmail.com>
2025-11-18 15:04:31 +0100

    going to try to use react!

---
+++ click and drag test/sketch.js
@@ -0,0 +1,108 @@
+// Star twinkle by wvnl on p5js.org; https://editor.p5js.org/wvnl/sketches
+
+var stars = [];
+var bigStars = [];
+
+function setup() {
+	createCanvas(1920, 1080);
+	//starImg = loadImage('/assets/star.png');
+	for (var i = 0; i < 3000; i++) {
+		stars[i] = new Star();
+	}
+
+	for (var i = 0; i < 500; i++) {
+		bigStars[i] = new bigStar();
+	}
+
+}
+
+function draw() {
+	resizeCanvas(windowWidth, windowHeight);
+	let time = frameCount * 0.001
+	let r = noise(0, 1, time) * 6 + 6;
+	let g = noise(2, 3, time) * 6 + 6;
+	let b = noise(4, 5, time) * 22 + 22;
+	let a = 255;
+	background(r, g, b, a);
+	cursor('/assets/star.png');
+	for (var i = 0; i < stars.length; i++) {
+		stars[i].draw();
+	}
+
+	for (var i = 0; i < bigStars.length; i++) {
+		bigStars[i].draw();
+	}
+	fill('white')
+	//circle(width / 2, height / 2, 4)
+
+  //
+  bigDipper(400, 600);
+}
+
+let img;
+
+function keyPressed() {
+  if (key === 's') {
+    img.save();
+  } else if (key === 'j') {
+    img.save('sky.jpg');
+  } else if (key === 'p') {
+    saveCanvas();
+  }
+}
+
+// Star class
+class Star {
+	constructor() {
+		this.x = random(width);
+		this.y = random(height);
+		this.size = random(2, 4);
+		this.t = random(TAU);
+	}
+
+	draw() {
+		this.t += 0.1;
+		var scale = this.size + sin(this.t) / 4;
+		let opacity = this.size * 6;
+		noStroke();
+
+		// Highlight
+		fill(200, 195, 255, opacity);
+		ellipse(this.x, this.y, scale * 3, scale * 3);
+
+		// Star
+		fill(200, 195, 255, opacity * 10);
+		ellipse(this.x, this.y, scale, scale);
+	}
+}
+
+class bigStar {
+	constructor() {
+		this.x = random(width);
+		this.y = random(height);
+		this.size = random(0.05, 2);
+		this.t = random(TAU);
+	}
+
+	draw() {
+		this.t += 0.1;
+		var scale = this.size + sin(this.t) / 1000;
+		let opacity = this.size * 12;
+		noStroke();
+
+		// Highlight
+		fill(200, 195, 255, opacity);
+		ellipse(this.x, this.y, scale * 3, scale * 3);
+
+		// Star
+		fill(200, 195, 255, opacity * 10);
+		ellipse(this.x, this.y, scale, scale);
+	}
+}
+
+function bigDipper(x, y){
+  fill("white");
+  let bigDipper1 = circle(x, y, 5);
+}
+
+// when user left clicks bigDipper1, a line should appear.
